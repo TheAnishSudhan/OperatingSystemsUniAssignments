6617490c5924d67843db3248f8ed2c08f05157e5
Anish Sudhan <64979922+TheAnishSudhan@users.noreply.github.com>
2022-01-07 00:19:17 +0530

    Completed lab 3 - all algos

--- fcfs_algo2.c
+++ fcfs_algo2.c
@@ -1,50 +1,39 @@
 #include <stdio.h>
-int num_process, processes[10], exit_times[10], arrival_times[10], completion_times[10], burst_times[10], turnAround_times[10], temp[10], temp2[10];
+int num_process, processes[10], exit_times[10], arrival_times[10], waiting_times[10];
+int burst_times[10], turnAround_times[10], priority[10];
+int temp, temp2;
+//First come serve first serve algo with arrival time priority
 
-
-int exitTime(int burst_time, int i){
-  if (i==0)
-    return 0;
-  return (burst_times[i-1] + exit_times[i-1]);
+int exitTime(int i){
+  if ((i==0) || (arrival_times[i]>exit_times[i-1]))
+    return (arrival_times[i] + burst_times[i]);
+  else
+    return (exit_times[i-1] + burst_times[i]);
 }
 
-int completion_time(int i){
-  return exit_times[i] + burst_times[i];
+int turnAroundTime(int i){
+  return exit_times[i]-arrival_times[i];
 }
 
-int turnAround_time(int i){
-  return completion_times[i]-arrival_times[i];
+int waitingTime(int i){
+  return turnAround_times[i] - burst_times[i];
 }
 
-// void avgTime(){
-//   int totalWaitingTime=0, totalTurnAroundTime=0;
-//   for (int i = 0; i < num_process; i++) {
-//     totalWaitingTime+=waiting_times[i];
-//     totalTurnAroundTime+=turnAround_times[i];
-//   }
-//   float avgWaitingTime= (float)totalWaitingTime/(float)num_process;
-//   float avgTurnAroundTime=(float)totalTurnAroundTime/(float)num_process;
-//
-//   printf("\n\nThe Average Waiting Time is: %.2f\n",avgWaitingTime );
-//   printf("The Average Turn Around Time is: %.2f", avgTurnAroundTime );
-// }
 
-void reorderArrivalTime(){
-  for (int i = 1; i < num_process; i++) {
-    if (arrival_times[i]<arrival_times[i-1]) {
-      temp[i]=arrival_times[i-1];
-      arrival_times[i-1]=arrival_times[i];
-      arrival_times[i]=temp[i];
-
-      temp2[i]=burst_times[i-1];
-      burst_times[i-1]=burst_times[i];
-      burst_times[i]=temp2[i];
-      /* code */
-    }/* code */
+void avgTime(){
+  int totalWaitingTime=0, totalTurnAroundTime=0;
+  for (int i = 0; i < num_process; i++) {
+    totalWaitingTime+=waiting_times[i];
+    totalTurnAroundTime+=turnAround_times[i];
   }
+  float avgWaitingTime= (float)totalWaitingTime/(float)num_process;
+  float avgTurnAroundTime=(float)totalTurnAroundTime/(float)num_process;
+
+  printf("\n\nThe Average Turn Around Time is: %.2f\n", avgTurnAroundTime );
+  printf("The Average Waiting Time is: %.2f",avgWaitingTime );
 }
 
-void main(){
+int main(){
 
   printf("Enter the number of processes: " );
   scanf("%d",&num_process);
@@ -57,50 +46,54 @@ void main(){
     printf("Enter the arrival time for process %d: ",i+1 );
     scanf("%d", &arrival_times[i] );
 
+    printf("Enter priority for process %d \n(Enter 1 in case of no same arrival time) ",i+1 );
+    scanf("%d", &priority[i] );
+
   }
 
-  reorderArrivalTime();
+for (int j = 0; j < num_process; j++) {
+  for (int i = j; i < num_process; i++) {
+    if ((arrival_times[i]<arrival_times[j])||priority[i]<priority[j]) {
+      temp=arrival_times[i];
+      arrival_times[i]=arrival_times[j];
+      arrival_times[j]=temp;
+
+      temp2=burst_times[i];
+      burst_times[i]=burst_times[j];
+      burst_times[j]=temp2;
+    }
+  }
+}
+
 
   for (int i = 0; i < num_process; i++) {
 
-    exit_times[i]=exitTime(burst_times[i], i);
+    exit_times[i]=exitTime(i);
 
-    completion_times[i]=completion_time(i);
+    turnAround_times[i]=turnAroundTime(i);
 
-    turnAround_times[i]=turnAround_time(i);
+    waiting_times[i]=waitingTime(i);
   }
 
-  for (int i = 0; i < num_process; i++) {
-    printf("Exit Times\n" );
-    printf("%d\n", exit_times[i]);
+  // for (int i = 0; i < num_process; i++) {
+  //   printf("Exit Times\n" );
+  //   printf("%d\n", exit_times[i]);
+  //
+  //   printf("Turn Around Times\n" );
+  //   printf("%d\n", turnAround_times[i]);
+  //
+  //   printf("Waiting Times\n" );
+  //   printf("%d\n", waiting_times[i]);
+  //
+  //   printf("Burst times for process %d: ",i+1 );
+  //   printf("%d\n", burst_times[i] );
+  //
+  //   printf("Arrival times for process %d: ",i+1 );
+  //   printf("%d\n", arrival_times[i] );
+  //
+  // }
 
-  }
+  avgTime();
 
-  for (int i = 0; i < num_process; i++) {
-    printf("Completion Times\n" );
-    printf("%d\n", completion_times[i]);
-
-  }
-
-  for (int i = 0; i < num_process; i++) {
-    printf("Turn Around Times\n" );
-    printf("%d\n", turnAround_times[i]);
-  }
-
-  //avgTime();
-
-//average waiting time and waiting time
-//completion time
-//turn around time = completion time - arrival time
-// arrival time
-
-//pt 3
-// exit time = burst times of all thise ahead
-// for exit time, order as per lower range, then thsat would wait  time
-//ta time = exit time - arrival time
-//waiting time = ta time - burst time
-//
-
-//need to create a dummy set of arrays to do the computation and then input values in the og arrays
-
+  return 0;
 }
