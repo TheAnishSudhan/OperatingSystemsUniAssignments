a06d056ae9f3ce3653b6a0ed51dd8010eb6295ab
Anish Sudhan <64979922+TheAnishSudhan@users.noreply.github.com>
2022-01-18 12:45:59 +0530

    Completed Round robin algorithm, try implementing with queues later on.

--- priority_pre.c
+++ priority_pre.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int num_process, processes[10], exit_times[10], arrival_times[20], waiting_times[20];
 int burst_times[20], turnAround_times[20], priority[20], new_priority[20], process_id[20];
-int original_burst_times[10], original_arrival_times[10], original_exit_times[10];
 int temp, temp2;
-//priority
+//pre-emtive priority scheduking
+//this algo is tailored to the question, in an generalised version, the instances of "6" would be replaced with a variable
 
 int exitTime(int i){
-  if ((i==0) || (original_arrival_times[i]>original_exit_times[i-1]))
-    return (original_arrival_times[i] + original_burst_times[i]);
-  else
-    return (exit_times[i-1] + original_burst_times[i]);
+  if (i==0)
+    return (arrival_times[i] + burst_times[i]);
+  else{
+    return (exit_times[i-1] + burst_times[i]);}
 }
 
 int turnAroundTime(int i){
@@ -23,12 +23,14 @@ int waitingTime(int i){
 
 void avgTime(){
   int totalWaitingTime=0, totalTurnAroundTime=0;
-  for (int i = 0; i < num_process; i++) {
+  for (int i = 0; i < 6; i++) {
     totalWaitingTime+=waiting_times[i];
     totalTurnAroundTime+=turnAround_times[i];
   }
-  float avgWaitingTime= (float)totalWaitingTime/(float)num_process;
-  float avgTurnAroundTime=(float)totalTurnAroundTime/(float)num_process;
+  printf("TOTAL WT TIME: %d\n", totalWaitingTime);
+  printf("TOTOAL TA TIME: %d\n", totalTurnAroundTime );
+  float avgWaitingTime= (float)totalWaitingTime/(float)6;
+  float avgTurnAroundTime=(float)totalTurnAroundTime/(float)6;
 
   printf("\n\nThe Average Turn Around Time is: %.2f\n", avgTurnAroundTime );
   printf("The Average Waiting Time is: %.2f",avgWaitingTime );
@@ -36,12 +38,19 @@ void avgTime(){
 
 void makeSpace(int* array, int count){
 
-  int temp=array[count];
+  int temp=array[count], temp2=0;
 
   for (int i = count+1; i < 20; i++) {
     temp2=array[i];
     array[i]=temp;
     temp=temp2;
+  }
+}
+
+void removeSpace(int* array, int count){
+
+  for (int i = count; i < (20-count); i++) {
+    array[i]=array[i+1];
   }
 }
 
@@ -54,10 +63,10 @@ int main(){
   for (int i = 0; i < num_process; i++) {
 
     printf("Enter the burst time for process %d: ",i+1 );
-    scanf("%d", &original_burst_times[i] );
+    scanf("%d", &burst_times[i] );
 
     printf("Enter the arrival time for process %d: ",i+1 );
-    scanf("%d", &original_arrival_times[i] );
+    scanf("%d", &arrival_times[i] );
 
     printf("Enter priority for process %d: \n",i+1 );
     scanf("%d", &priority[i] );
@@ -66,50 +75,25 @@ int main(){
 
   }
 
-// for (int j = 0; j < num_process; j++) {
-//   for (int i = j; i < num_process; i++) {
-//     if (arrival_times[i]<arrival_times[j]) {
-//       temp=arrival_times[i];
-//       arrival_times[i]=arrival_times[j];
-//       arrival_times[j]=temp;
-//
-//       temp2=burst_times[i];
-//       burst_times[i]=burst_times[j];
-//       burst_times[j]=temp2;
-//     }
-//     if ((arrival_times[i]==arrival_times[j])&&priority[i]<priority[j]&&i<j) {
-//       printf("Change happening\n" );
-//       temp=arrival_times[i];
-//       arrival_times[i]=arrival_times[j];
-//       arrival_times[j]=temp;
-//
-//       temp2=burst_times[i];
-//       burst_times[i]=burst_times[j];
-//       burst_times[j]=temp2;
-//     }
-//   }
-// }
-
-
 for (int j = 0; j < num_process; j++) {
   for (int i = j; i < num_process; i++) {
     if (arrival_times[i]<arrival_times[j]) {
-      temp=original_arrival_times[i];
-      original_arrival_times[i]=original_arrival_times[j];
-      original_arrival_times[j]=temp;
+      temp=arrival_times[i];
+      arrival_times[i]=arrival_times[j];
+      arrival_times[j]=temp;
 
-      temp2=original_burst_times[i];
-      original_burst_times[i]=original_burst_times[j];
-      original_burst_times[j]=temp2;
+      temp2=burst_times[i];
+      burst_times[i]=burst_times[j];
+      burst_times[j]=temp2;
     }
     if ((arrival_times[i]==arrival_times[j])&&priority[i]<priority[j]) {
-      temp=original_arrival_times[i];
-      original_arrival_times[i]=original_arrival_times[j];
-      original_arrival_times[j]=temp;
+      temp=arrival_times[i];
+      arrival_times[i]=arrival_times[j];
+      arrival_times[j]=temp;
 
-      temp2=original_burst_times[i];
-      original_burst_times[i]=original_burst_times[j];
-      original_burst_times[j]=temp2;
+      temp2=burst_times[i];
+      burst_times[i]=burst_times[j];
+      burst_times[j]=temp2;
 
       temp2=priority[i];
       priority[i]=priority[j];
@@ -118,83 +102,74 @@ for (int j = 0; j < num_process; j++) {
   }
 }
 
-for (int i = 0; i < num_process; i++) {
-
-  original_exit_times[i]=exitTime(i);
-  exit_times[i]=original_exit_times[i];
-  printf("Exit time: %d\n", original_exit_times[i]);
-
-  arrival_times[i]=original_arrival_times[i];
-
-  new_priority[i]=priority[i];
-
-  original_burst_times[i]=burst_times[i];
-
+for (int i = 0; i < 6; i++) {
+  exit_times[i]=exitTime(i);
 }
 
-for (int i = 0; i < 20; i++) {
-    if ((exit_times[i]>original_arrival_times[i+1])&&(new_priority[i]<new_priority[i+1])) {
+int og_burst_time=0;
 
-      makeSpace(&original_burst_times, i+2);
+for (int i = 0; i < 6; i++) {
+    if ((exit_times[i]>arrival_times[i+1])&&(priority[i]>priority[i+1])&&(burst_times[i]!=0)&&(burst_times[i+1]!=0)) {
+
+      makeSpace(&burst_times, i+2);
       makeSpace(&exit_times, i+2);
-      makeSpace(&original_arrival_times, i+2);
-      makeSpace(&new_priority, i+2);
+      makeSpace(&arrival_times, i+2);
+      makeSpace(&priority, i+2);
       makeSpace(&process_id, i+2);
 
-      original_burst_times[i+2]=exit_times[i]-arrival_times[i]+1;
-      original_arrival_times[i+2]=original_arrival_times[i];
-      new_priority[i+2]=new_priority[i];
+      og_burst_time=burst_times[i];
+      if ((arrival_times[i+1]-exit_times[i-1])<0) {
+        burst_times[i]=0;
+      } else {
+        burst_times[i]=arrival_times[i+1]-exit_times[i-1];
+      }
+      if (burst_times[i]==0) {
+        burst_times[i+2]=og_burst_time;
+      } else {
+        burst_times[i+2]=og_burst_time-burst_times[i];
+      }
+      arrival_times[i+2]=arrival_times[i];
+      priority[i+2]=priority[i];
       process_id[i+2]=process_id[i];
-      exit_times[i+2]=exitTime(i+2);
+      exit_times[i]=exitTime(i);
 
-      printf("Process %d runs now\n",process_id[i] );
-      printf("The i running now is: %d \n",i );
+      if (burst_times[i]==0) {
+        removeSpace(&burst_times, i);
+        removeSpace(&exit_times, i);
+        removeSpace(&arrival_times, i);
+        removeSpace(&priority, i);
+        removeSpace(&process_id, i);
 
-
+        exit_times[i]=exitTime(i);
+      }
   }
-}
-
-//print the process getting executed-> maybe ccreate a process_id[] array
-//create a new set of arrays for burst time etc and then use them for computing averages
-
-
-for (int i = 0; i < num_process; i++) {
-
-  printf("Exit time: %d\n", exit_times[i]);
-
-  arrival_times[i];
-
-  new_priority[i];
-
-  original_burst_times[i];
-
-}
-
-
-  for (int i = 0; i < num_process; i++) {
-
+  else {
     exit_times[i]=exitTime(i);
+  }
+
+}
+
+
+  for (int i = 0; i < 6; i++) {
 
     turnAround_times[i]=turnAroundTime(i);
 
     waiting_times[i]=waitingTime(i);
   }
 
-  for (int i = 0; i < num_process; i++) {
-    printf("Exit Times: " );
-    printf("%d\n", exit_times[i]);
+  for (int i = 0; i < 6; i++) {
 
-    printf("Turn Around Times: " );
-    printf("%d\n", turnAround_times[i]);
+    printf("PROCESS RUNNING: %d\n",process_id[i] );
 
-    printf("Waiting Times: " );
-    printf("%d\n", waiting_times[i]);
+    printf("Exit Time: %d\n", exit_times[i]);
 
-    printf("Burst times for process %d: ",i+1 );
-    printf("%d\n", burst_times[i] );
+    printf("Turn Around Time: %d\n", turnAround_times[i]);
 
-    printf("Arrival times for process %d: ",i+1 );
-    printf("%d\n", arrival_times[i] );
+    printf("Waiting Time: %d\n",waiting_times[i]);
+
+    printf("Burst time: %d\n",burst_times[i]);
+
+    printf("Arrival time: %d\n",arrival_times[i]);
 
   }
 
--- round_robin.c
+++ round_robin.c
@@ -1,57 +1,48 @@
 #include <stdio.h>
-int num_process, processes[10], exit_times[10], arrival_times[20], waiting_times[20];
-int burst_times[20], turnAround_times[20], priority[20], new_priority[20], process_id[20];
-int temp, temp2;
-//pre-emtive priority scheduking
-//this algo is tailored to the question, in an generalised version, the instances of "6" would be replaced with a variable
+int num_process, time_slice, processes[10], exit_times[10], arrival_times[20], waiting_times[20];
+int burst_times[20], turnAround_times[20], priority[20], new_priority[20], process_id[20], og_burst_times[20];
+int temp, temp2, length=0, length2=0, count=0, temp_burst_times[20], final_exit_times[20];;
+//round robin algorithm
 
 int exitTime(int i){
   if (i==0)
     return (arrival_times[i] + burst_times[i]);
-  else{
+  else {
     return (exit_times[i-1] + burst_times[i]);}
 }
 
 int turnAroundTime(int i){
-  return exit_times[i]-arrival_times[i];
+  return final_exit_times[i]-arrival_times[i];
 }
 
 int waitingTime(int i){
-  return turnAround_times[i] - burst_times[i];
+  return turnAround_times[i] - og_burst_times[i];
 }
-
 
 void avgTime(){
   int totalWaitingTime=0, totalTurnAroundTime=0;
-  for (int i = 0; i < 6; i++) {
+  for (int i = 0; i < num_process; i++) {
     totalWaitingTime+=waiting_times[i];
     totalTurnAroundTime+=turnAround_times[i];
   }
-  printf("TOTAL WT TIME: %d\n", totalWaitingTime);
+  printf("\n\nTOTAL WT TIME: %d\n", totalWaitingTime);
   printf("TOTOAL TA TIME: %d\n", totalTurnAroundTime );
-  float avgWaitingTime= (float)totalWaitingTime/(float)6;
-  float avgTurnAroundTime=(float)totalTurnAroundTime/(float)6;
+  float avgWaitingTime= (float)totalWaitingTime/(float)num_process;
+  float avgTurnAroundTime=(float)totalTurnAroundTime/(float)num_process;
 
   printf("\n\nThe Average Turn Around Time is: %.2f\n", avgTurnAroundTime );
   printf("The Average Waiting Time is: %.2f",avgWaitingTime );
 }
 
-void makeSpace(int* array, int count){
-
-  int temp=array[count], temp2=0;
-
-  for (int i = count+1; i < 20; i++) {
-    temp2=array[i];
-    array[i]=temp;
-    temp=temp2;
-  }
+void addProcess(int* array, int i){
+  array[num_process+length]=array[i];
+  count++;
+  length=count/3;
 }
 
-void removeSpace(int* array, int count){
-
-  for (int i = count; i < (20-count); i++) {
-    array[i]=array[i+1];
-  }
+void addProcessBurst(int* array, int burst_time){
+  array[num_process+length2]=burst_time;
+  length2++;
 }
 
 int main(){
@@ -59,90 +50,54 @@ int main(){
   printf("Enter the number of processes: " );
   scanf("%d",&num_process);
 
+  printf("Enter the time slice: ");
+  scanf("%d",&time_slice);
+
 
   for (int i = 0; i < num_process; i++) {
 
     printf("Enter the burst time for process %d: ",i+1 );
     scanf("%d", &burst_times[i] );
 
+    og_burst_times[i]=burst_times[i];
+    temp_burst_times[i]=burst_times[i];
+
     printf("Enter the arrival time for process %d: ",i+1 );
     scanf("%d", &arrival_times[i] );
-
-    printf("Enter priority for process %d: \n",i+1 );
-    scanf("%d", &priority[i] );
 
     process_id[i]=i+1;
 
   }
 
-for (int j = 0; j < num_process; j++) {
-  for (int i = j; i < num_process; i++) {
-    if (arrival_times[i]<arrival_times[j]) {
-      temp=arrival_times[i];
-      arrival_times[i]=arrival_times[j];
-      arrival_times[j]=temp;
+//iterator calc
 
-      temp2=burst_times[i];
-      burst_times[i]=burst_times[j];
-      burst_times[j]=temp2;
-    }
-    if ((arrival_times[i]==arrival_times[j])&&priority[i]<priority[j]) {
-      temp=arrival_times[i];
-      arrival_times[i]=arrival_times[j];
-      arrival_times[j]=temp;
+int iterator=0, temp_iterator=0;
 
-      temp2=burst_times[i];
-      burst_times[i]=burst_times[j];
-      burst_times[j]=temp2;
-
-      temp2=priority[i];
-      priority[i]=priority[j];
-      priority[j]=temp2;
+for (int i = 0; i < num_process; i++) {
+  while (temp_burst_times[i]>time_slice) {
+    if (temp_burst_times[i]>time_slice) {
+      temp_burst_times[i]-=time_slice;
+      temp_iterator++;
     }
   }
 }
 
-for (int i = 0; i < 6; i++) {
-  exit_times[i]=exitTime(i);
-}
+iterator=num_process+temp_iterator;
 
-int og_burst_time=0;
+int og_burst_time=0, future_burst_time=0;
 
-for (int i = 0; i < 6; i++) {
-    if ((exit_times[i]>arrival_times[i+1])&&(priority[i]>priority[i+1])&&(burst_times[i]!=0)&&(burst_times[i+1]!=0)) {
-
-      makeSpace(&burst_times, i+2);
-      makeSpace(&exit_times, i+2);
-      makeSpace(&arrival_times, i+2);
-      makeSpace(&priority, i+2);
-      makeSpace(&process_id, i+2);
-
+for (int i = 0; i < iterator; i++) {
+    if (burst_times[i]>time_slice) {
 
       og_burst_time=burst_times[i];
-      if ((arrival_times[i+1]-exit_times[i-1])<0) {
-        burst_times[i]=0;
-      } else {
-        burst_times[i]=arrival_times[i+1]-exit_times[i-1];
-      }
-      if (burst_times[i]==0) {
-        burst_times[i+2]=og_burst_time;
-      } else {
-        burst_times[i+2]=og_burst_time-burst_times[i];
-      }
-      arrival_times[i+2]=arrival_times[i];
-      priority[i+2]=priority[i];
-      process_id[i+2]=process_id[i];
+      burst_times[i]=time_slice;
+      future_burst_time=og_burst_time-burst_times[i];
       exit_times[i]=exitTime(i);
 
-      if (burst_times[i]==0) {
-        removeSpace(&burst_times, i);
-        removeSpace(&exit_times, i);
-        removeSpace(&arrival_times, i);
-        removeSpace(&priority, i);
-        removeSpace(&process_id, i);
-
-        exit_times[i]=exitTime(i);
-      }
+      addProcessBurst(&burst_times, future_burst_time);
+      addProcess(&arrival_times, i);
+      addProcess(&priority, i);
+      addProcess(&process_id, i);
   }
   else {
     exit_times[i]=exitTime(i);
@@ -150,51 +105,39 @@ for (int i = 0; i < 6; i++) {
 
 }
 
-//
-// for (int i = 0; i < 6; i++) {
-//   printf("Exit time: %d\n", exit_times[i]);
-// }
-//
-// for (int i = 0; i < 6; i++) {
-//   printf("Arrival time: %d\n", arrival_times[i]);
-// }
-//
-// for (int i = 0; i < 6; i++) {
-//   printf("Burst time: %d\n",   burst_times[i]);
-// }
-//
-// for (int i = 0; i < 6; i++) {
-//   printf("Priority: %d\n", priority[i]);
-// }
-//
-// for (int i = 0; i < 6; i++) {
-//   printf("Process running: %d \n",process_id[i] );
-// }
-
-
-  for (int i = 0; i < 6; i++) {
-
-    //exit_times[i]=exitTime(i);
-
-    turnAround_times[i]=turnAroundTime(i);
-
-    waiting_times[i]=waitingTime(i);
+for (int i = 0; i < num_process; i++) {
+  for (int j = 0; j < iterator; j++) {
+    if (process_id[j]==(i+1)) {
+      final_exit_times[i]=exit_times[j];
+    }
   }
+}
 
-  for (int i = 0; i < 6; i++) {
+printf("\n\n");
+
+  for (int i = 0; i < iterator; i++) {
 
     printf("PROCESS RUNNING: %d\n",process_id[i] );
 
-    printf("Exit Time: %d\n", exit_times[i]);
-
-    printf("Turn Around Time: %d\n", turnAround_times[i]) );
-
-    printf("Waiting Time: %d\n",waiting_times[i]));
-
     printf("Burst time: %d\n",burst_times[i]);
 
-    printf("Arrival time: %d\n",arrival_times[i] );
   }
+
+  printf("\n\n");
+
+    for (int i = 0; i < num_process; i++) {
+
+      turnAround_times[i]=turnAroundTime(i);
+
+      waiting_times[i]=waitingTime(i);
+
+      printf("\nProcess %d\n",i+1);
+
+      printf("Turn Around Time: %d\n", turnAround_times[i]);
+
+      printf("Waiting Time: %d\n",waiting_times[i]);
+    }
+
 
   avgTime();
 
